68339acf4121f8cf480f1482557b6225d7482308
DaroMacs <dariomacedog@gmail.com>
2025-04-07 22:33:50 -0600

    Initial setup of Next.js app with UI components and theme support

---
+++ src/components/Hero.tsx
@@ -0,0 +1,52 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="#4bb6d3"
+        />
+        <Spotlight
+          className="left-80 top-28 h-[80vh] w-[50vw]"
+          fill="#6ef2cc"
+        />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          <TextGenerateEffect
+            words="Transforming service blockchainpayments into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider my-9 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Dario, a Next.js Developer testing.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Connect"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
